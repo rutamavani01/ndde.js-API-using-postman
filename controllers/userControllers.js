9a9502af60992977c179b1e0150541e853d1743d
Ruta Mavani <you@example.com>
2024-02-21 10:45:49 +0530

    change-pass

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const login = async(req,res)=> {
     try {
+
         // let hash = await bcrypt.hash(password,10);        l
         let user = await userModel.findOne({email : req.body.email});
         if(!user || user.password != req.body.password){
@@ -29,6 +30,16 @@ const login = async(req,res)=> {
 
 const registerUser = async(req,res) => {
     try {
+
+        let dup_user = await userModel.findOne({email : req.body.email});
+
+        if(dup_user){
+            return res.status(400).send({
+                success : false,
+                message : "user allready registered try another unique email address"
+            })
+        }
+
        let user = await userModel.create({
             name : req.body.name,
             email : req.body.email,
@@ -47,29 +58,64 @@ const registerUser = async(req,res) => {
 }
 
 const changePassword = async(req,res) => {
-    const { name, oldPassword, newPassword } = req.body;
-    try {
-        let user = await userModel.findOne({ name });
+    // const { name, oldPassword, newPassword } = req.body;
+    // try {
+    //     let user = await userModel.findOne({ name });
 
-        if (!user || user.password !== oldPassword) {
-            return res.status(401).send({
-                success: false,
-                message: 'Invalid username or incorrect oldpassword'
-            });
-        }
+    //     if (!user || user.password !== oldPassword) {
+    //         return res.status(401).send({
+    //             success: false,
+    //             message: 'Invalid username or incorrect oldpassword'
+    //         });
+    //     }
 
-        user.password = newPassword;    
-        await user.save();
+    //     user.password = newPassword;    
+    //     await user.save();
 
-        return res.send({
-            success: true,
-            message: 'Password changed successfully'
-        });
+    //     return res.send({
+    //         success: true,
+    //         message: 'Password changed successfully'
+    //     });
         
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
+    // } catch (error) {
+    //     console.log(error);
+    //     return false;
+    // }
+
+        try {
+            let email = req.body.email;
+            let user = userModel.findOne({email : req.body.email});
+
+            if(!user){
+                return res.status(400).send({
+                    success : true,
+                    message : "not valid email address"
+                })
+            }
+
+            let newPassword = req.body.newpassword;
+            let cpassword = req.body.cpassword;
+
+            if(newPassword == cpassword){
+                let update = await userModel.findByIdAndUpdate(req.query.id,{
+                    password : newPassword,
+                })
+                return res.status(200).send({
+                    success : true,
+                    message : "password changed",
+                })
+                
+            }else{
+                return res.status(400).send({
+                    success : true,
+                    message : "password and confirm password is not match"
+                })
+            }
+
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
 }
   
 module.exports = {    
